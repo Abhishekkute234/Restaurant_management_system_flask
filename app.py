521f202d2a81a3378f6776b2045e6ad7cd927586
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-09-09 07:36:23 +0530

    add new files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return render_template('index.html')
+
+  
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/booking')
+def booking_page():
+    return render_template('booking.html')
+  
+@app.route('/contact')
+def route_page():
+    return render_template('contact.html')      
+  
+@app.route('/menu')
+def menu_page():
+    return render_template('menu.html')       
+  
+if __name__=="__main__":
+    app.run(debug=True)   
