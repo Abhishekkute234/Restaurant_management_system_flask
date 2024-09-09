0d853256fbb691e90bd7c2e2143c6cee467c67da
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-09-09 17:00:56 +0530

    updating files

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
     return render_template('index.html')
 
-  
 @app.route('/about')
 def about_page():
     return render_template('about.html')
@@ -13,14 +13,14 @@ def about_page():
 @app.route('/booking')
 def booking_page():
     return render_template('booking.html')
-  
+
 @app.route('/contact')
-def route_page():
-    return render_template('contact.html')      
-  
+def contact_page():
+    return render_template('contact.html')
+
 @app.route('/menu')
 def menu_page():
-    return render_template('menu.html')       
-  
-if __name__=="__main__":
-    app.run(debug=True)   
+    return render_template('menu.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
